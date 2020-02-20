cafa9f21c63511ed5c59bb78b55e2ad2902806d2
albertzed <mralbertzheng@gmail.com>
2020-02-19 20:53:53 -0500

    Added ButtonClass and MouseClickHandler files

---
+++ src/com/minesweeper/ButtonClass.java
@@ -0,0 +1,127 @@
+//name this file ButtonClass
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.minesweeper;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import static com.minesweeper.MineSweeperPart1.theGame;
+
+/**
+ *
+ * @author albert.zheng
+ */
+public class ButtonClass extends JButton {
+    int value;
+    private int[] location;
+    private boolean exposed;
+    private boolean isFlagged;
+
+    public ButtonClass()
+
+    {
+        setPreferredSize(new Dimension(30, 30));
+        exposed = false;
+        isFlagged = false;
+    }
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+    public int getValue()
+    {
+        return value;
+    }
+    public void setLocation(int row, int col)
+    {
+        location = new int[2];
+        location[0] = row;
+        location[1] = col;
+    }
+    public int[] getLocat()
+    {
+        return location;
+    }
+    public void expose()
+    {
+        exposed = true;
+        theGame.nonMineTiles--;
+        if (value==0)
+        {
+            setIcon(theGame.MINE_ICON);
+        }
+        else if (value == 1)
+        {
+            setIcon(theGame.TILE_ONE);
+        }
+        else if (value == 2)
+        {
+            setIcon(theGame.RED_MINE_ICON);
+        }
+        else if (value == 3)
+        {
+            setIcon(theGame.TILE_THREE);
+        }
+        else if (value == 4)
+        {
+            setIcon(theGame.TILE_FOUR);
+        }
+        else if (value == 5)
+        {
+            setIcon(theGame.TILE_FIVE);
+        }
+        else if (value == 6)
+        {
+            setIcon(theGame.TILE_SIX);
+        }
+        else if (value == 7)
+        {
+            setIcon(theGame.TILE_SEVEN);
+        }
+        else if (value == 8)
+        {
+            setIcon(theGame.TILE_EIGHT);
+        }
+        else if (value == -1)
+        { //if a mine is exposed, end the game
+            if (isFlagged)
+            {
+                setIcon(theGame.RED_MINE_ICON);
+                theGame.nonMineTiles++;
+                return;
+            }
+            else
+            {
+                setIcon(theGame.MINE_ICON);
+                theGame.nonMineTiles++;
+            }
+        }
+
+        if (theGame.nonMineTiles == 0)
+        {
+            theGame.winGame();
+        }
+    }
+    public boolean isExposed()
+    {
+        return exposed;
+    }
+
+    public void flag()
+    {
+        if (isFlagged == false)
+        {
+            isFlagged = true;
+        }
+        else
+        {
+            isFlagged = false;
+        }
+    }
+    public boolean isFlagged()
+    {
+        return isFlagged;
+    }
+}
---
+++ src/com/minesweeper/MouseClickHandler.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.minesweeper;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import static com.minesweeper.MineSweeperPart1.theGame;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author albert.zheng
+ */
+public class MouseClickHandler implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me)
+    {
+        //checks which buttons have been clicked
+        ButtonClass but;
+        but = (ButtonClass) me.getSource(); //gets source of clicked button
+
+        //check for right-click only condition
+        if (me.isMetaDown())
+        {
+            if (but.isExposed() == false)
+            {
+                but.flag();
+                if (but.isFlagged())
+                {
+                    but.setIcon(theGame.FLAG);
+                }
+                else
+                {
+                    but.setIcon(theGame.CLOSED_TILE);
+                }
+            }
+        }
+        if (SwingUtilities.isLeftMouseButton(me)) //check if left mouse button has been pressed 
+        {
+            if(SwingUtilities.isRightMouseButton(me))//check if right mouse button is also being pressed
+            {   //if they're both pressed, then call a function that exposes surrouunding unflagged and covered tiles
+                theGame.exposeSurroundingUnflagged(but.getLocat());
+            }
+            if (but.isFlagged() == false)
+            {
+                but.expose();
+                if (but.value == -1)
+                {
+                    theGame.endGame();
+                    but.setIcon(theGame.SAD_MINE_ICON);
+                }
+                else if (but.value == 0)
+                {
+                    theGame.exposeSurroundingZeros(but.getLocat());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+    }
+
+}
+//name this file MouseClickHandler
